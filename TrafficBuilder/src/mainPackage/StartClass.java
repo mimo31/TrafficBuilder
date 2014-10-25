9bcf58f129abeb931e0b3e0d2d000bbff48895f6
mimo31 <weweuuuu@email.cz>
2014-10-25 19:56:29 +0200

    Centering the title

--- TrafficBuilder/src/mainPackage/StartClass.java
+++ TrafficBuilder/src/mainPackage/StartClass.java
@@ -21,13 +21,25 @@ public class StartClass {
 	public static class paintIt extends JComponent{
 		public void paint(Graphics g){
 			Graphics2D graph2 = (Graphics2D)g;
+			/*int counter = 1;
+			while(counter < 101){
+			graph2.setFont(Variables.nowUsingFont.deriveFont(Font.BOLD, counter));
+			System.out.println(Functions.getStringBounds(graph2, "Traffic Builder", 0, 0).width + " - " + counter);
+			counter++;
+			}
+			System.exit(0);*/
 			graph2.fill(new Rectangle(Variables.width / 4, Variables.height / 4, Variables.width / 2, Variables.height / 16));
 			graph2.fill(new Rectangle(Variables.width / 4, Variables.height / 32 * 11, Variables.width / 2, Variables.height / 16));
 			graph2.fill(new Rectangle(Variables.width / 4, Variables.height / 16 * 7, Variables.width / 2, Variables.height / 16));
-			graph2.setFont(Variables.nowUsingFont.deriveFont(Font.BOLD, 8f));
+			graph2.setFont(Variables.nowUsingFont.deriveFont(Font.BOLD, 101f));
 			Rectangle s1Size = new Rectangle(Functions.getStringBounds(graph2, "Traffic Builder", 0, 0));
-			graph2.setFont(Variables.nowUsingFont.deriveFont(Font.BOLD, (float) (Variables.width / 2) / s1Size.width * 8));
-			graph2.drawString("Traffic Builder", Variables.width / 4 , 100);
+			Double s1Per1Width = (double) (s1Size.width / 101);
+			Double s1Per1Height = (double) (s1Size.height / 101);
+			s1Size = null;
+			graph2.setFont(Variables.nowUsingFont.deriveFont(Font.BOLD, (float) (Variables.width / 2 / s1Per1Width)));
+			graph2.drawString("Traffic Builder", (Variables.width - Functions.getStringBounds(graph2, "Traffic Builder", 0, 0).width) / 2 , 100);
+			System.out.println(Variables.width / 2);
+			System.out.println(Functions.getStringBounds(graph2, "Traffic Builder", 0, 0).width);
 		}
 	}
 }
